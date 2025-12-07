b3d19116ca881af771901a16b4f1ec5f4d786926
Chhuangg <Ly.Chhuang@student.greenriver.edu>
2025-12-06 19:40:13 -0800

    .

---
+++ potato.js
@@ -0,0 +1,11 @@
+function toggleChange(){
+    document.body.classList.toggle("potatoChange");
+}
+
+function revealFact(){
+   document.getElementById('faq-answer').classList.toggle('hidden');
+}
+
+function changeFont(){
+    document.body.classList.toggle("fontChange");
+}
